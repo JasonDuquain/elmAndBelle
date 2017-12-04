9de846f4a70e104d404668dcc2e06c6c5c6d5adc
jasonduquain <jduquain@gmail.com>
2017-12-03 20:58:08 -0500

    basic site complete w/o MQs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
+/*******    STICKY HEADER    *********/
 let hdr = document.querySelector('header');
 let fullNav = document.querySelector('.fullNavWrapper');
 
-document.addEventListener('scroll', () => {
+function stickee() {
     let navBottom = hdr.getBoundingClientRect().bottom;
     if (navBottom <= 450) {
         fullNav.classList.add('sticky');
     } else {
         fullNav.classList.remove('sticky');
     }
-});
+}
+    
+document.addEventListener('scroll', stickee);
+
+
+/*******    HIGHLIGHT ON SCROLL    *********/
+let sects = document.querySelectorAll('section:not(.intro)');
+let anchs = document.querySelectorAll('.navs a:not(.logoAnch)');
+
+function scrollHighlight() {
+     anchs.forEach((el, i) => {
+        if (sects[i].getBoundingClientRect().top <= 50 && sects[i].getBoundingClientRect().bottom >= 50) {
+            el.classList.add('scrollHighlight');
+        } else {
+            el.classList.remove('scrollHighlight');
+        }
+    });
+}
+
+document.addEventListener('scroll', scrollHighlight);
+
+
+/*********   UPDATE COPYRIGHT YEAR   ***********/
+let dateSpan = document.querySelector('span');
+
+dateSpan.textContent = new Date().getFullYear();
+
 
 
 
